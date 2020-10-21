2a8f5c5cbd4338e2453f51368051d249179f4e74
alessandroibrahim99 <38700012+alessandroibrahim99@users.noreply.github.com>
2020-10-21 14:47:10 -0400

    Update config.py

--- settings/config.py
+++ settings/config.py
@@ -12,9 +12,9 @@ port = int(os.environ.get("PORT", 5000))
 
 debug = False
 
-contacts = "https://www.linkedin.com/in/mauro-di-pietro-56a1366b/"
+contacts = "https://www.linkedin.com/in/alessandro-ibrahim-b24640a/"
 
-code = "https://github.com/mdipietro09/App_VirusForecaster"
+#code = "https://github.com/mdipietro09/App_VirusForecaster"
 
 fontawesome = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
 
